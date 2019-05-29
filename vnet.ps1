cef5735f36fe1bde670ab8be4f28693fc106840c
CheeseBreadIsGood <49957396+CheeseBreadIsGood@users.noreply.github.com>
2019-05-29 14:22:06 -0400

    Update vnet.ps1

--- vnet.ps1
+++ vnet.ps1
@@ -1,29 +1,29 @@
 ﻿# Add-AzureRmAccount
 
-$rgName="rangervnetrg"
+$rgName="testtesttest"
 $location="eastus2"
 
 # Create a resource group.
-New-AzureRmResourceGroup -Name $rgName -Location $location
+New-AZResourceGroup -Name $rgName -Location $location
 
 # Subnet configuration
-$subnet1config = New-AzureRmVirtualNetworkSubnetConfig -Name "mysubnet1" -AddressPrefix "10.3.1.0/24"
-$subnet2config = New-AzureRmVirtualNetworkSubnetConfig -Name "mysubnet2" -AddressPrefix "10.3.2.0/24"
+$subnet1config = New-AZVirtualNetworkSubnetConfig -Name "mysubnet1" -AddressPrefix "10.3.1.0/24"
+$subnet2config = New-AZVirtualNetworkSubnetConfig -Name "mysubnet2" -AddressPrefix "10.3.2.0/24"
 
 
 # Create the VNet with the subnet configurations
-$vnet = New-AzureRmVirtualNetwork -ResourceGroupName $rgName -Name "myvnet" -AddressPrefix '10.3.0.0/16' -Location $location -Subnet $subnet1config, $subnet2config
+$vnet = New-AZVirtualNetwork -ResourceGroupName $rgName -Name "myvnet" -AddressPrefix '10.3.0.0/16' -Location $location -Subnet $subnet1config, $subnet2config
 
 
 # Create Public IP addresses for the virtual machines
-$server1pubip = New-AzureRmPublicIpAddress -ResourceGroupName $rgName -Name "server1-pubip" -location $location -AllocationMethod Dynamic 
-$server2pubip = New-AzureRmPublicIpAddress -ResourceGroupName $rgName -Name "server2-pubip" -location $location -AllocationMethod Dynamic
-$server3pubip = New-AzureRmPublicIpAddress -ResourceGroupName $rgName -Name "server3-pubip" -location $location -AllocationMethod Dynamic
+$server1pubip = New-AzPublicIpAddress -ResourceGroupName $rgName -Name "server1-pubip" -location $location -AllocationMethod Dynamic 
+$server2pubip = New-AzPublicIpAddress -ResourceGroupName $rgName -Name "server2-pubip" -location $location -AllocationMethod Dynamic
+$server3pubip = New-AzPublicIpAddress -ResourceGroupName $rgName -Name "server3-pubip" -location $location -AllocationMethod Dynamic
 
 # Create NICs for the virtual machines
-$server1nic = New-AzureRmNetworkInterface -ResourceGroupName $rgName -Location $location -Name "server1-nic"  -Subnet $vnet.Subnets[0] -PublicIpAddress $server1pubip -PrivateIpAddress "10.3.1.5"
-$server2nic = New-AzureRmNetworkInterface -ResourceGroupName $rgName -Location $location -Name "server2-nic"  -Subnet $vnet.Subnets[0] -PublicIpAddress $server2pubip -PrivateIpAddress "10.3.1.6"
-$server3nic = New-AzureRmNetworkInterface -ResourceGroupName $rgName -Location $location -Name "server3-nic"  -Subnet $vnet.Subnets[1] -PublicIpAddress $server3pubip -PrivateIpAddress "10.3.2.5"
+$server1nic = New-AzNetworkInterface -ResourceGroupName $rgName -Location $location -Name "server1-nic"  -Subnet $vnet.Subnets[0] -PublicIpAddress $server1pubip -PrivateIpAddress "10.3.1.5"
+$server2nic = New-AzNetworkInterface -ResourceGroupName $rgName -Location $location -Name "server2-nic"  -Subnet $vnet.Subnets[0] -PublicIpAddress $server2pubip -PrivateIpAddress "10.3.1.6"
+$server3nic = New-AzNetworkInterface -ResourceGroupName $rgName -Location $location -Name "server3-nic"  -Subnet $vnet.Subnets[1] -PublicIpAddress $server3pubip -PrivateIpAddress "10.3.2.5"
 
 
 ############################################################################
@@ -33,43 +33,43 @@ $server3nic = New-AzureRmNetworkInterface -ResourceGroupName $rgName -Location $
 $servercred = Get-Credential -Message "Enter a username and password for the servers"
 
 # Create Server 1, 2 & 3
-$server1vmConfig = New-AzureRmVMConfig -VMName "server1" -VMSize "Standard_DS2" | `
-  Set-AzureRmVMOperatingSystem -Windows -ComputerName "server1" -Credential $servercred | `
-  Set-AzureRmVMSourceImage -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2016-Datacenter" -Version latest | `
-  Set-AzureRmVMBootDiagnostics -Disable | Add-AzureRmVMNetworkInterface -Id $server1nic.Id 
+$server1vmConfig = New-AzVMConfig -VMName "server1" -VMSize "Standard_DS2" | 
+  Set-AzVMOperatingSystem -Windows -ComputerName "server1" -Credential $servercred | 
+  Set-AzVMSourceImage -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2019-Datacenter" -Version latest | 
+  Set-AzVMBootDiagnostics -Disable | Add-AzVMNetworkInterface -Id $server1nic.Id 
 
-$server2vmConfig = New-AzureRmVMConfig -VMName "server2" -VMSize "Standard_DS2" | `
-  Set-AzureRmVMOperatingSystem -Windows -ComputerName "server2" -Credential $servercred | `
-  Set-AzureRmVMSourceImage -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2016-Datacenter" -Version latest | `
-  Set-AzureRmVMBootDiagnostics -Disable | Add-AzureRmVMNetworkInterface -Id $server2nic.Id 
+$server2vmConfig = New-AzVMConfig -VMName "server2" -VMSize "Standard_DS2" | 
+  Set-AzVMOperatingSystem -Windows -ComputerName "server2" -Credential $servercred | 
+  Set-AzVMSourceImage -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2019-Datacenter" -Version latest | 
+  Set-AzVMBootDiagnostics -Disable | Add-AzVMNetworkInterface -Id $server2nic.Id 
 
-$server3vmConfig = New-AzureRmVMConfig -VMName "server3" -VMSize "Standard_DS2" | `
-  Set-AzureRmVMOperatingSystem -Windows -ComputerName "server3" -Credential $servercred | `
-  Set-AzureRmVMSourceImage -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2016-Datacenter" -Version latest | `
-  Set-AzureRmVMBootDiagnostics -Disable | Add-AzureRmVMNetworkInterface -Id $server3nic.Id 
+$server3vmConfig = New-AzVMConfig -VMName "server3" -VMSize "Standard_DS2" | 
+  Set-AzVMOperatingSystem -Windows -ComputerName "server3" -Credential $servercred | 
+  Set-AzVMSourceImage -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2019-Datacenter" -Version latest | 
+  Set-AzVMBootDiagnostics -Disable | Add-AzVMNetworkInterface -Id $server3nic.Id 
 
 
 
-$server1vm = New-AzureRmVM -ResourceGroupName $rgName -Location $location -VM $server1vmConfig 
-$server2vm = New-AzureRmVM -ResourceGroupName $rgName -Location $location -VM $server2vmConfig
-$server3vm = New-AzureRmVM -ResourceGroupName $rgName -Location $location -VM $server3vmConfig
+$server1vm = New-AzVM -ResourceGroupName $rgName -Location $location -VM $server1vmConfig 
+$server2vm = New-AzVM -ResourceGroupName $rgName -Location $location -VM $server2vmConfig
+$server3vm = New-AzVM -ResourceGroupName $rgName -Location $location -VM $server3vmConfig
 
 
 # Remove Resource Group
-# Remove-AzureRmResourceGroup -Name $rgName
+# Remove-AzResourceGroup -Name $rgName
 
 
 #############################################################
 # Some helpful cmdlets 
 
 ## Get all resopurce groups in the Subscription
-# Get-AzureRmResourceGroup
+# Get-AzResourceGroup
 
 ## Get Azure VNet information
-# Get-AzureRmVirtualNetwork -ResourceGroupName rangervnetrg -Name myvnet
+# Get-AzVirtualNetwork -ResourceGroupName rangervnetrg -Name myvnet
 
 ## Get all Azure Public IP Addresses
-# Get-AzureRmPublicIpAddress
+# Get-AzPublicIpAddress
 
 ## Get all Azure Network Interfaces 
-# Get-AzureRmNetworkInterface
+# Get-AzNetworkInterface
